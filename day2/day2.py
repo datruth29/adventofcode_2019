029d816bbf986e96f21c10c4155943feba1e033b
Adam Collado <adam.collado@gmail.com>
2019-12-08 16:25:53 -0500

    Finished day2

--- day2/day2.py
+++ day2/day2.py
@@ -10,5 +10,27 @@ def process_file(filename):
     return data
 
 
-if x == 1:
-    
+def generate(x, y, data):
+    data[1] = x
+    data[2] = y
+    for i in range(0, len(data), 4):
+        element = data[i]
+        if element == 99:
+            break
+        if element == 1:
+            first_pos = data[i+1]
+            second_pos = data[i+2]
+            third_pos = data[i+3]
+            data[third_pos] = data[first_pos] + data[second_pos]
+        if element == 2:
+            first_pos = data[i+1]
+            second_pos = data[i+2]
+            third_pos = data[i+3]
+            data[third_pos] = data[first_pos] * data[second_pos]
+
+    return data[0]
+
+
+data = process_file(DATAFILE)
+print(generate(77, 33, data[:]))
+print((100 * 77) + 33)
